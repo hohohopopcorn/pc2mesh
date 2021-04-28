6a7927d073d5161076c0b0eb5f1010335ddc62db
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2021-04-27 22:15:38 -0700

    updates

--- johnvm/poisson.py
+++ johnvm/poisson.py
@@ -5,34 +5,64 @@ import matplotlib.pyplot as plt
 # from functools import total_ordering
 
 
+EPSILON = 1.00e-8  # a small number
+ALPHA = 1.00e24    # a very large number
+
+
 @dataclass
 class Point:
     """
-    Oriented point.
+    Point object.
     """
     # Location
-    x: float
-    y: float
-    z: float
-    # Normal
-    nx: float
-    ny: float
-    nz: float
+    x: float = field(default=0.00)
+    y: float = field(default=0.00)
+    z: float = field(default=0.00)
 
     def __eq__(self, other):
         return (self.x == other.x and
                 self.y == other.y and
                 self.z == other.z)
 
+    def __add__(self, other):
+        """
+        add points with "+".
+        """
+        x = self.x + other.x
+        y = self.y + other.y
+        z = self.z + other.z
+        return(Point(x, y, z))
+
+    def __mul__(self, c: float):
+        """
+        multiply with a scalar using "*".
+        """
+        x = self.x * c
+        y = self.y * c
+        z = self.z * c
+        return(Point(x, y, z))
+
 
 @dataclass
-class Points:
+class Oriented_Point(Point):
+    """
+    Oriented point.
+    """
+
+    # Normal
+    nx: float = field(default=0.00)
+    ny: float = field(default=0.00)
+    nz: float = field(default=0.00)
+
+
+@dataclass
+class Oriented_Points:
     """
     Collector for unique oriented points.
     """
-    points: List[Point] = field(default_factory=list)
+    points: List[Oriented_Point] = field(default_factory=list)
 
-    def add(self, pt: Point):
+    def add(self, pt: Oriented_Point):
         """
         Add a point in the list of points,
         if it is not already in
@@ -43,7 +73,57 @@ class Points:
             raise ValueError('Oriented point already exists: '
                              + repr(pt))
 
+    def centroid(self):
+        """
+        Compute the centroid of the points
+        """
+        centroid = Point()
+        for pt in self.points:
+            centroid += pt
+        centroid = centroid * (1.00/float(len(self.points)))
+        return centroid
+
+    def bbox_dim(self):
+        """
+        Compute the dimensions of the bouding box
+        """
+        xs = [pt.x for pt in self.points]
+        ys = [pt.y for pt in self.points]
+        zs = [pt.z for pt in self.points]
+        dX = max(xs) - min(xs)
+        dY = max(ys) - min(ys)
+        dZ = max(zs) - min(zs)
+        return (dX, dY, dZ)
+
+    def split_along_plane(self,
+                          center: Point,
+                          plane: str):
+        group1 = Oriented_Points()
+        group2 = Oriented_Points()
+        if plane == "xy":
+            for pt in self.points:
+                if pt.z < center.z:
+                    group1.add(pt)
+                else:
+                    group2.add(pt)
+        elif plane == "xz":
+            for pt in self.points:
+                if pt.y < center.y:
+                    group1.add(pt)
+                else:
+                    group2.add(pt)
+        elif plane == "yz":
+            for pt in self.points:
+                if pt.x < center.x:
+                    group1.add(pt)
+                else:
+                    group2.add(pt)
+        return (group1, group2)
+
     def show(self):
+        """
+        Show a 3D scatter polot of the points
+        """
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         x_coords = []
@@ -70,6 +150,10 @@ class Points:
             )
         np.savetxt(file_name+'.npts', point_matrix, delimiter=' ')
 
+    def __repr__(self):
+        return str(len(self.points)) + " points."
+
+
 ##########
 # OCTREE #
 ##########
@@ -80,25 +164,224 @@ class Node:
     """
     Octree node.
     """
+    depth: int
+
     # is it a leaf node?
     leaf: bool
 
-    # center of the cube
+    # center of the node
     center: Point
     # width of the cube
     width: float
 
+    # oriented points contained in the node
+    contents: Oriented_Points
+
     # association with other nodes
     parent: 'Node'
-    children: List['Node']
-    depth: int
+    children: List['Node'] = field(default_factory=list)
 
-    # points contained in the node
-    contents: List[Point]
+    def subdivide(self, max_depth):
+        """
+        Subdivide `depth` times
+        """
+        pts = self.contents
+        # If the node contains any points
+        if pts.points:
+            # Must subdivide.
+            # Make children:
+            self.contents = None
+            self.leaf = False
+            # compute lower-left-front corner
+            # (to obtain child centerpoints more easily)
+            dlf_point = self.center + Point(*[-self.width/2]*3)
+            # Splitting the containing oriented points
+            # split along yz (into top-bottom quadrants)
+            down_up = pts.split_along_plane(
+                self.center, 'xy')
+            for z, du in enumerate(down_up):
+                # and then along zx (into front-back pairs)
+                front_back = du.split_along_plane(
+                    self.center, 'xz')
+                for y, fb in enumerate(front_back):
+                    # and then along xy (into left and right cube)
+                    left_right = fb.split_along_plane(
+                        self.center, 'yz')
+                    for x, lr in enumerate(left_right):
+                        # compute initialization parameters
+                        # and instantiate new leaf nodes
+                        child_depth = self.depth + 1
+                        child_width = self.width/2.00
+                        child_center = dlf_point + Point(
+                            x * child_width + child_width/2.00,
+                            y * child_width + child_width/2.00,
+                            z * child_width + child_width/2.00
+                        )
+                        child_node = Node(
+                            child_depth,
+                            True,
+                            child_center,
+                            child_width,
+                            lr,
+                            self
+                        )
+                        self.children.append(child_node)
+                        # recursively subdivide:
+                        if child_depth < max_depth:
+                            child_node.subdivide(max_depth)
+
+    def leaf_nodes(self):
+        """
+        Return all leaf nodes connected to this parent node
+        (directly or further down the tree)
+        """
+        node = self  # start here
+        leaves = []  # instantiate an empty array
+        if node.leaf:  # if it's already a leaf
+            leaves.append(self)
+        else:
+            # it's not a leaf. go down recursively and check the children
+            for child in self.children:
+                for leaf in child.leaf_nodes():
+                    leaves.append(leaf)
+        # at this point we have collected all the leaves
+        return leaves
+
+    def lines_to_plot(self):
+        """
+        (For plotting the cube)
+        """
+        return ([
+            [[self.center.x - self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y - self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z - self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x - self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y - self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y - self.width/2.00],
+             [self.center.z + self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x + self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y - self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y + self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z - self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x - self.width/2.00],
+             [self.center.y + self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y + self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z + self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x + self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y + self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x - self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z + self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x + self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z + self.width/2.00,
+                self.center.z + self.width/2.00]],
+            [[self.center.x - self.width/2.00,
+                self.center.x - self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z - self.width/2.00]],
+            [[self.center.x + self.width/2.00,
+                self.center.x + self.width/2.00],
+             [self.center.y - self.width/2.00,
+                self.center.y + self.width/2.00],
+             [self.center.z - self.width/2.00,
+                self.center.z - self.width/2.00]],
+        ])
+
+    def __repr__(self):
+        return str(id(self))
 
 
 @dataclass
 class Octree:
     """
-    Octree spatial partition
+    Octree spatial partition.
+    Parameters:
+        Points (Points): Points object
+        depth (int): Maximum depth of the tree
+    Returns:
+        Octree object
     """
+    points: Oriented_Points
+    depth: int
+    head: Node = field(init=False)
+    leaf_nodes: List[Node] = field(default_factory=list)
+
+    def __post_init__(self):
+        # instantiate the head node
+        self.head = Node(
+            0,
+            True,
+            self.points.centroid(),
+            max(self.points.bbox_dim()),
+            self.points,
+            None
+        )
+        # subdivide `depth` times
+        self.head.subdivide(self.depth)
+        self.leaf_nodes = self.head.leaf_nodes()
+
+    def show(self):
+        """
+        ! WARNING ! For small cases only!
+        Makes a plot showing the boundaries of the
+        octree leaf nodes and the points
+        """
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        kwargs_node = {'alpha': 0.50, 'color': 'black'}
+
+        for lines in self.head.lines_to_plot():
+            ax.plot3D(*lines, **kwargs_node)
+
+        for leaf in self.leaf_nodes:
+
+            for lines in leaf.lines_to_plot():
+                ax.plot3D(*lines, **kwargs_node)
+            x_coords = []
+            y_coords = []
+            z_coords = []
+            for pt in leaf.contents.points:
+                x_coords.append(pt.x)
+                y_coords.append(pt.y)
+                z_coords.append(pt.z)
+            ax.scatter(x_coords, y_coords, z_coords)
+        plt.show()
--- johnvm/test.py
+++ johnvm/test.py
@@ -1,5 +1,10 @@
+import matplotlib.pyplot as plt
+from johnvm.poisson import Point, Oriented_Point, Oriented_Points, Octree, Node
 import numpy as np
-from poisson import Point, Points
+import os
+
+import time
+# os.chdir("./johnvm")
 
 
 def test_generate_sphere(n_pts=1000):
@@ -7,19 +12,58 @@ def test_generate_sphere(n_pts=1000):
     Generate oriented points sampled from a sphere
     centered at (0,0,0) with radius 1
     """
-    pts = Points()  # initialize collector of points
+    pts = Oriented_Points()  # initialize collector of points
     # use numpy to sample points on a sphere
     point_matrix = np.random.normal(size=(n_pts, 3))
     for i in range(n_pts):
         pt_data = point_matrix[i, :] / \
             np.linalg.norm(point_matrix[i, :])  # normalize
         # add in the point list
-        pts.add(Point(pt_data[0], pt_data[1], pt_data[2],
-                pt_data[0], pt_data[1], pt_data[2]))
+        pts.add(Oriented_Point(pt_data[0], pt_data[1], pt_data[2],
+                               pt_data[0], pt_data[1], pt_data[2]))
     return pts
 
 
-pts = test_generate_sphere()
+pts = test_generate_sphere(1000)
 
 # pts.show()
 # pts.to_npts_file("lala")
+
+# pts.centroid()
+# max(pts.bbox_dim())
+
+
+def get_volume(pts, size):
+    octree = Octree(pts, size)
+    volume = 0.00
+    for leaf in octree.leaf_nodes:
+        if len(leaf.contents.points) > 0:
+            volume += leaf.width**3
+    return volume
+
+
+def get_n_leaves(pts, size):
+    octree = Octree(pts, size)
+    n = 0
+    for leaf in octree.leaf_nodes:
+        if len(leaf.contents.points) > 0:
+            n += 1
+    return n
+
+
+tic = time.perf_counter()
+
+
+sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# volumes = [get_volume(pts, size) for size in sizes]
+leaves = [get_n_leaves(pts, size) for size in sizes]
+
+toc = time.perf_counter()
+print(f"Execution time: {toc - tic:0.4f} seconds")
+
+plt.figure()
+plt.plot(sizes, leaves)
+plt.show()
+
+
+# octree.show()
